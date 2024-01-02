10ed6ad02a4fac8c85275843bfbc8d849dad7ef1
Demetry Pascal <qtckpuhdsa@gmail.com>
2024-01-02 23:04:02 +0300

    -

--- main.py
+++ main.py
@@ -4,52 +4,58 @@ from pathlib import Path
 from collections import Counter
 import pickle
 
+import numpy as np
+
 
 def compress(text: str, iterations: int = 10000):
 
-    table = []
-    text = list(text)
+    symbols = ''.join(k for k, _ in Counter(text).most_common())
 
-    for k in range(1, iterations + 1):
-        if len(text) < 1:
+    s_map = {k: i for i, k in enumerate(symbols, 1)}
+
+    arr = np.array(
+        [s_map[s] for s in text]
+    )
+
+    n_map = []
+    s_count = len(symbols) + 1
+
+    for k in range(s_count, s_count + iterations):
+        if arr.size < 2:
             break
 
-        (v1, v2), count = Counter(zip(text[:-1], text[1:])).most_common(1)[0]
-        if count < 2:
+        (v1, v2), count = Counter(zip(arr[:-1], arr[1:])).most_common(1)[0]
+        if count < 4:
             break
-        table.append((k, (v1, v2)))
 
-        t = []
+        n_map.extend((-k, v1, v2) if v1 != v2 else (k, v1))
+
+        _arr = np.empty_like(arr)
         last_ok_i = -1
-        last_index = len(text) - 1
+        last_index = arr.size - 1
 
-        i = 0
+        i = j = 0
         while i < last_index:
-            s = text[i]
-            if s == v1 and text[i + 1] == v2:
-                t.append(k)
+            s = arr[i]
+
+            if s == v1 and arr[i + 1] == v2:
+                _arr[j] = k
                 last_ok_i = i + 1
                 i += 2
             else:
-                t.append(s)
+                _arr[j] = s
                 i += 1
 
+            j += 1
+
         if last_ok_i != last_index:  # last is not appended
-            t.append(text[-1])
+            _arr[j] = arr[last_index]
 
-        text = t
+        arr = _arr[:j+1]
 
-    print(f"iterations = {k + 1}")
+    print(f"iterations = {k - s_count}")
 
-    t = [text[0]]
-    for v in text[1:]:
-        if isinstance(v, str) and isinstance(t[-1], str):
-            t[-1] += v
-        else:
-            t.append(v)
-    text = t
-
-    return table, text
+    return symbols, np.array(n_map).astype(np.int16), arr.astype(np.int16)
 
 
 def int_to_bytes(v: int):
@@ -58,23 +64,19 @@ def int_to_bytes(v: int):
 
 
 if __name__ == '__main__':
-    tb, tt = compress(
+    ss, nm, rr = compress(
         # 'curl --proxy http://localhost:8051 https://www.baidu.com --include --verbose'
         Path('paths.yaml').read_text(encoding='utf-8')
     )
 
-    import numpy as np
-
-    np.savez(
+    np.savez_compressed(
         'res.npz',
-        arr=np.array(
-            #sum(([k, v1, v2] for k, (v1, v2) in tb), []) + [0] + tt,
-            tt,
-            dtype=object
-        )
+        symbols=np.array([ss]),
+        maps=nm,
+        text=rr
     )
 
-    print()
+    print(Path('res.npz').stat().st_size)
     # txt = '&'.join(
     #     f"{k}${('' if isinstance(v1, int) else '.') + str(v1)}${('' if isinstance(v2, int) else '.') + str(v2)}"
     #     for k, (v1, v2) in tb
